b1a3992d416bf1620d07289cbbcaaf840f893529
Glitch (remix-maquetando01) <none>
2018-07-11 15:35:10 +0000

    queda en coment segunda ventana

---
+++ cliente.js
@@ -0,0 +1,41 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+
+
+
+class Reposteria {
+  
+  static publicar(){
+    console.log('[..] Buenos Aires Programando Online!!');
+    
+    //alert(12345678);
+    
+    //document.querySelector('#panelMensaje').innerHTML += "Algo! ";
+    
+    //document.querySelector('section > h3').innerHTML = "Algo! ";
+    
+    //alert( document.querySelector('#panelMensaje')    );
+    //alert( document.querySelector('#producto_titulo') );
+    
+    document.querySelector('#panelMensaje').innerHTML += document.querySelector('#producto_titulo').value;
+    //document.querySelector('#panelMensaje').innerHTML += document.querySelector('#producto_producto').value;
+    
+    
+    console.log('[OK] Buenos Aires Programando Online!!');
+  }
+  
+  // static publicar2(){
+    //console.log('[..] Buenos Aires Programando Online!!');
+    
+    //document.querySelector('#panelMensaje').innerHTML += document.querySelector('#producto_producto').value;
+    
+    
+    //console.log('[OK] Buenos Aires Programando Online!!'); 
+  }
+  
+}
+
+
+
